cc4737615180d9311cf356510419730107247c95
Noé BAIOCCHI <n.baiocchi@groupeonepoint.com>
2024-01-16 15:50:02 +0100

    add EF Core in the project

---
+++ NewStart/NewStart.Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewStart.Server.Models;
+
+namespace NewStart.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoModel> Todo { get; set; }
+    }
+}
---
+++ NewStart/NewStart.Server/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NewStart.Server.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
